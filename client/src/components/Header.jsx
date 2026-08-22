82cb7a46bac927cddfed4df1031e6ce9ce7e47e0
bdutt001 <bdutt001@odu.edu>
2026-08-22 14:00:38 -0400

    changed styling of glass effect

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,14 @@ import './header.css';
 import Icon from "../assets/favicon.svg";
 
 export default function Header() {
-
   return (
     <header className="header">
-        <div className="row name">
-          <img src={Icon} className="header-icon"/>
-          <h1>Benjamin Dutton</h1>
+        <div className="header__title">
+          <img src={Icon} className="header__icon"/>
+          <div className="header__column">
+            <h1>Benjamin Dutton</h1>
+            <p>Software Engineer and Web Developer</p>
+          </div>
         </div>
         <Nav />
     </header>
