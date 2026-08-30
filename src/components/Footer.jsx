188c7646a5b364ba16f91720fc3b135c98f80aaa
bdutt001 <bdutt001@odu.edu>
2026-08-29 22:17:19 -0400

    added copyright statement to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import './footer.css';
 export default function Footer() {
     return (
         <div className="footer">
+            <p className='copyright'>© 2026 Benjamin Dutton. All rights reserved.</p>
         </div>
     );
 }
